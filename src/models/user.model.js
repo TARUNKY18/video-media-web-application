da66c5498d949d057acf719c5f0a02e78f6039dc
TARUNKY18 <kunduryogesh@gmail.com>
2024-05-28 19:41:28 +0530

    starting with controllers and routes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
     userName : {
         type: String,
         required: true,
